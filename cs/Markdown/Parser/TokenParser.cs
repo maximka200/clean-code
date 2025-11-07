1814946be9e0f89caad46abce1f9bdfed9bea4e7
maximka200 <sterben8912@gmail.com>
2025-11-07 18:12:27 +0500

    - Написал тесты - Написал базовую реализацию в декларативном стиле и без некоторой логики, поэтому не все тесты проходят

--- cs/Markdown/Parser/TokenParser.cs
+++ cs/Markdown/Parser/TokenParser.cs
@@ -1,15 +1,176 @@
-﻿using Markdown.Domain;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Markdown.Domain;
+using Markdown.Domains.NodeExtentions;
 
-namespace Markdown.Parser;
-
-/// <summary>
-/// Собирает токены в дерево Node 
-/// </summary>
-public static class TokenParser
+namespace Markdown.Parser
 {
-    public static Node Parse(List<MdToken> tokens)
+    public static class TokenParser
     {
-        // TODO: Implement TokenParser.Parse
-        throw new NotImplementedException();
+        public static Node Parse(List<MdToken> tokens)
+        {
+            var rootChildren = new List<Node>();
+            var i = 0;
+            while (i < tokens.Count)
+            {
+                var token = tokens[i];
+
+                // Headers 
+                if (token.Type == TokenType.Grid)
+                {
+                    var level = 0;
+                    while (i < tokens.Count && tokens[i].Type == TokenType.Grid)
+                    {
+                        level++;
+                        i++;
+                    }
+
+                    // Header must have space after hashes
+                    if (i < tokens.Count && tokens[i].Type == TokenType.Space)
+                    {
+                        i++; // skip space
+                        var contentNodes = new List<Node>();
+                        while (i < tokens.Count && tokens[i].Type != TokenType.Escape)
+                        {
+                            contentNodes.Add(new TextNode(tokens[i].Value));
+                            i++;
+                        }
+
+                        rootChildren.Add(new HeaderNode(level, contentNodes));
+                        continue;
+                    }
+                    // not a valid header — treat as text
+                    rootChildren.Add(new TextNode(new string('#', level)));
+                    continue;
+                }
+
+                // --- New line ---
+                if (token.Type == TokenType.Escape)
+                {
+                    rootChildren.Add(new Node(NodeType.NewLine));
+                    i++;
+                    continue;
+                }
+
+                // --- Escaped characters ---
+                if (token.Type == TokenType.Slash)
+                {
+                    if (i + 1 < tokens.Count)
+                    {
+                        var next = tokens[i + 1];
+                        rootChildren.Add(new TextNode("\\" + next.Value));
+                        i += 2;
+                    }
+                    else
+                    {
+                        rootChildren.Add(new TextNode("\\"));
+                        i++;
+                    }
+                    continue;
+                }
+
+                // --- Bold / Italic ---
+                if (token.Type == TokenType.Underscore)
+                {
+                    var underscoreCount = 0;
+                    while (i < tokens.Count && tokens[i].Type == TokenType.Underscore)
+                    {
+                        underscoreCount++;
+                        i++;
+                    }
+                    
+                    // check on space after underscore
+                    var spaceCountAfter = 0;
+                    while (i < tokens.Count && tokens[i].Type == TokenType.Space)
+                    {
+                        spaceCountAfter++;
+                        i++;
+                    }
+
+                    if (spaceCountAfter != 0)
+                    {
+                        AddSymbol("_", underscoreCount, rootChildren);
+                        AddSymbol(" ", underscoreCount, rootChildren);
+                        continue;
+                    }
+                    
+                    // italic
+                    if (underscoreCount == 1)
+                    {
+                        var closeIndex = FindClosing(tokens, i, "_");
+                        
+                        if (closeIndex != -1)
+                        {
+                            var inner = Parse(tokens.GetRange(i, closeIndex - i));
+                            rootChildren.Add(new Node(NodeType.Italic, inner.Children));
+                            i = closeIndex + 1;
+                            continue;
+                        }
+                        AddSymbol("_", underscoreCount, rootChildren);
+                        continue;
+                    }
+                    
+                    // bold
+                    if (underscoreCount == 2)
+                    {
+                        var closeIndex = FindClosing(tokens, i, "__");
+                        if (closeIndex != -1)
+                        {
+                            var inner = Parse(tokens.GetRange(i, closeIndex - i));
+                            rootChildren.Add(new Node(NodeType.Bold, inner.Children));
+                            i = closeIndex + 2;
+                            continue;
+                        }
+                        AddSymbol("_", underscoreCount, rootChildren);
+                        continue;
+                    }
+                    
+                    AddSymbol("_", underscoreCount, rootChildren);
+                    continue;
+                }
+
+                // --- Default: text tokens ---
+                if (token.Type == TokenType.Word ||
+                    token.Type == TokenType.Number ||
+                    token.Type == TokenType.Space)
+                {
+                    rootChildren.Add(new TextNode(token.Value));
+                    i++;
+                    continue;
+                }
+
+                // fallback
+                rootChildren.Add(new TextNode(token.Value));
+                i++;
+            }
+
+            return new Node(NodeType.Root, rootChildren);
+        }
+
+        private static void AddSymbol(string symbol, int count, List<Node> root)
+        {
+            for (var _ = 0; _ < count; _++)
+                root.Add(new TextNode(symbol));
+        }
+
+        private static int FindClosing(List<MdToken> tokens, int startIndex, string pattern)
+        {
+            for (var j = startIndex; j < tokens.Count - pattern.Length + 1; j++)
+            {
+                var match = true;
+                for (var k = 0; k < pattern.Length; k++)
+                {
+                    if (tokens[j + k].Type != TokenType.Underscore)
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                    return j;
+            }
+            return -1;
+        }
     }
 }
--- cs/MarkdownTests/TokenParserTests.cs
+++ cs/MarkdownTests/TokenParserTests.cs
@@ -1,6 +1,492 @@
-namespace MarkdownTest;
+using System.Collections.Generic;
+using FluentAssertions;
+using Markdown.Domain;
+using Markdown.Domains.NodeExtentions;
+using Markdown.Parser;
+using NUnit.Framework;
 
+namespace Markdown.Tests;
+
+[TestFixture]
 public class TokenParserTests
 {
+    public static IEnumerable<TestCaseData> TokenParserCases_Default()
+    {
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Grid, "#"),
+                new(TokenType.Space, " "),
+                new(TokenType.Word, "Test")
+            },
+            new Node(NodeType.Root,  new List<Node>
+            {
+                new HeaderNode(1,new List<Node>
+                {
+                    new TextNode("Test")
+                })
+            })
+        ).SetName("HeaderNode_Level1");
+        
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Grid, "#"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Space, " "),
+                new(TokenType.Word, "Test")
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new HeaderNode(2,new List<Node>
+                {
+                    new TextNode( "Test")
+                })
+            })
+        ).SetName("HeaderNode_Level2");
+        
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Grid, "#"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Space, " "),
+                new(TokenType.Word, "Test")
+            },
+            new Node(NodeType.Root,  new List<Node>
+            {
+                new HeaderNode(6,  new List<Node>
+                {
+                    new TextNode( "Test")
+                })
+            })
+        ).SetName("HeaderNode_Level6");
+        
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "Test"),
+                new(TokenType.Underscore, "_")
+            },
+            new Node(NodeType.Root,  new List<Node>
+            {
+                new(NodeType.Italic,  new List<Node>
+                {
+                    new TextNode( "Test")
+                })
+            })
+        ).SetName("ItalicUnderscoreNode");
+        
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Number, "123"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+            },
+            new Node(NodeType.Root,  new List<Node>
+            {
+                new(NodeType.Bold,  new List<Node>
+                {
+                    new TextNode( "123")
+                })
+            })
+        ).SetName("BoldUnderscoreNode");
+        
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "Test"),
+                new(TokenType.Underscore, "_")
+            },
+            new Node(NodeType.Root,  new List<Node>
+            {
+                new(NodeType.Italic,  new List<Node>
+                {
+                    new TextNode( "Test")
+                })
+            })
+        ).SetName("ItalicUnderscoreNode");
+        
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Number, "123"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+            },
+            new Node(NodeType.Root,  new List<Node>
+            {
+                new(NodeType.Bold,  new List<Node>
+                {
+                    new TextNode("123")
+                })
+            })
+        ).SetName("BoldUnderscoreNode");
+        
+        
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "Test"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Escape, "\n"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Space, " "),
+                new(TokenType.Word, "Header"),
+                new(TokenType.Space, " "),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Escape, "\n")
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new(NodeType.Italic, new List<Node>
+                {
+                    new TextNode("Test")
+                }),
+                new(NodeType.NewLine),
+                new HeaderNode(2, new List<Node>
+                {
+                    new TextNode("Header"),
+                    new TextNode(" "),
+                    new TextNode("##"),
+                    new TextNode(" "),
+                }),
+                new(NodeType.NewLine)
+            })
+        ).SetName("WithDifferentTags");
+    }
+
+    public static IEnumerable<TestCaseData> TokenParserCases_Extreme()
+    {
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "UnclosedItalic")
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new TextNode("_"),
+                new TextNode("UnclosedItalic")
+            })
+        ).SetName("UnclosedItalic");
+        
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "__"),
+                new(TokenType.Word, "UnclosedBold")
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new TextNode("__"),
+                new TextNode("UnclosedBold")
+            })
+        ).SetName("UnclosedBold");
+        
+        // Любой символ можно экранировать, чтобы он не считался частью разметки.
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Slash, "\\"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "SlashUnderscore"),
+                new(TokenType.Slash, "\\"),
+                new(TokenType.Underscore, "_")
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new TextNode("\\_"),
+                new TextNode("SlashUnderscore"),
+                new TextNode("\\_")
+            })
+        ).SetName("SlashUnderscoreBold_BothSides");
+        
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Slash, "\\"),
+                new(TokenType.Grid, "#"),
+                new(TokenType.Space, " "),
+                new(TokenType.Underscore, "HEADER")
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new TextNode("\\# "),
+                new TextNode(" "),
+                new TextNode("HEADER"),
+            })
+        ).SetName("SlashBeforeGrid");
+        
+        
+        // Если внутри подчерков пустая строка ____, то они остаются символами подчерка.
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new TextNode("_"),
+                new TextNode("_"),
+                new TextNode("_"),
+                new TextNode("_"),
+            })
+        ).SetName("FourSlash");
+        
+        
+        // __Непарные_ символы в рамках одного абзаца не считаются выделением.
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "Непарные"),
+                new(TokenType.Underscore, "_"),
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new TextNode("_"),
+                new TextNode("_"),
+                new TextNode("Непарные"),
+                new TextNode("_"),
+            })
+        ).SetName("Unpaired");
+        
+        // Внутри __двойного выделения _одинарное_ тоже__ работает
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "test"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "test"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "test"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new(NodeType.Bold, new List<Node>
+                {
+                    new TextNode("test"),
+                    new(NodeType.Italic, new List<Node>
+                    {
+                        new TextNode("test")
+                    })
+                })
+            })).SetName("WithinDoubleSelectionSingle");
+        
+        // Но не наоборот — внутри _одинарного __двойное__ не_ работает.
+        yield return new TestCaseData(
+            new List<MdToken>
+            {
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "test"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "test"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Underscore, "_"),
+                new(TokenType.Word, "test"),
+                new(TokenType.Underscore, "_"),
+            },
+            new Node(NodeType.Root, new List<Node>
+            {
+                new(NodeType.Italic, new List<Node>
+                {
+                    new TextNode("_"),
+                    new TextNode("test"),
+                    new TextNode("_"),
+                    new TextNode("_"),
+                    new TextNode("test"),
+                    new TextNode("_"),
+                    new TextNode("_"),
+                    new TextNode("test"),
+                    new TextNode("_"),
+                })
+            })).SetName("WithinSingleSelectionDouble");
+        
+            // Подчерки внутри текста c цифрами_12_3 не считаются выделением и должны оставаться символами подчерка
+            yield return new TestCaseData(
+                new List<MdToken>
+                {
+                    new(TokenType.Word, "цифрами"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Number, "12"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Number, "3"),
+                },
+                new Node(NodeType.Root, new List<Node>
+                {
+                    new TextNode("цифрами_12_3"),
+                })
+            ).SetName("UnderscoreInNumbers");
+            
+            // Однако выделять часть слова они могут: и в _нач_але, и в сер_еди_не, и в кон_це._
+            yield return new TestCaseData(
+                new List<MdToken>
+                {
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "нач"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "але"),
+                },
+                new Node(NodeType.Root, new List<Node>
+                {
+                    new(NodeType.Italic, new List<Node> 
+                        {
+                            new TextNode("нач")
+                        }
+                    ),
+                    new TextNode("але")
+                })
+            ).SetName("UnderscoreInWord_Start");
+            
+            yield return new TestCaseData(
+                new List<MdToken>
+                {
+                    new(TokenType.Word, "сер"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "eди"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "не"),
+                },
+                new Node(NodeType.Root, new List<Node>
+                {
+                    new TextNode("сер"),
+                    new(NodeType.Italic, new List<Node> 
+                        {
+                            new TextNode("eди")
+                        }
+                    ),
+                    new TextNode("не")
+                })
+            ).SetName("UnderscoreInWord_Middle");
+            
+            yield return new TestCaseData(
+                new List<MdToken>
+                {
+                    new(TokenType.Word, "кон"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "це"),
+                    new(TokenType.Underscore, "_"),
+                },
+                new Node(NodeType.Root, new List<Node>
+                {
+                    new TextNode("кон"),
+                    new(NodeType.Italic, new List<Node> 
+                        {
+                            new TextNode("це")
+                        }
+                    )
+                })
+            ).SetName("UnderscoreInWord_Finish");
+            
+            // В то же время выделение в ра_зных сл_овах не работает
+            yield return new TestCaseData(
+                new List<MdToken>
+                {
+                    new(TokenType.Word, "ра"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "зных"),
+                    new(TokenType.Space, " "),
+                    new(TokenType.Word, "сло"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "вах"),
+                },
+                new Node(NodeType.Root, new List<Node>
+                {
+                    new TextNode("ра_зных сл_овах"),
+                })
+            ).SetName("UnderscoreDifferentWord");
+            
+            // эти_ подчерки_ и эти _подчерки _ не считаются выделением, 
+            yield return new TestCaseData(
+                new List<MdToken>
+                {
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Space, " "),
+                    new(TokenType.Word, "подчерки"),
+                    new(TokenType.Underscore, "_"),
+                },
+                new Node(NodeType.Root, new List<Node>
+                {
+                    new TextNode("_"),
+                    new TextNode(" "),
+                    new TextNode("подчерки"),
+                    new TextNode("_"),
+                })
+            ).SetName("SpaceAfterOpenUnderscore");
+            
+            yield return new TestCaseData(
+                new List<MdToken>
+                {
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "подчерки"),
+                    new(TokenType.Space, " "),
+                    new(TokenType.Underscore, "_"),
+                },
+                new Node(NodeType.Root, new List<Node>
+                {
+                    new TextNode("_"),
+                    new TextNode("подчерки"),
+                    new TextNode(" "),
+                    new TextNode("_"),
+                })
+            ).SetName("SpaceBeforeClosedUnderscore");
+            
+            // В случае __пересечения _двойных__ и одинарных_ подчерков ни один из них не считается выделением.
+            yield return new TestCaseData(
+                new List<MdToken>
+                {
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "пересечения"),
+                    new(TokenType.Space, " "),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Word, "двойных"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Underscore, "_"),
+                    new(TokenType.Space, " "),
+                    new(TokenType.Word, "и"),
+                    new(TokenType.Space, " "),
+                    new(TokenType.Word, "одинарных"),
+                    new(TokenType.Underscore, "_"),
+                },
+                new Node(NodeType.Root, new List<Node>
+                {
+                    new TextNode("__пересечения _двойных__ и одинарных_"),
+                })
+            ).SetName("UnionDoubleAndOneUnderscore");
+    }
     
+    [Test]
+    [TestCaseSource(nameof(TokenParserCases_Default))]
+    [TestCaseSource(nameof(TokenParserCases_Extreme))]
+    public void Parse_ShouldParse_Correctly(List<MdToken> tokens, Node expectedNode)
+    {
+        var result = TokenParser.Parse(tokens);
+
+        result.Should().BeEquivalentTo(expectedNode);
+    }
 }
